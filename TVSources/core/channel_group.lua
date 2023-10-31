bd06415e68b7d4e0619aa3231519520c80586e68
longhai <lelonghai@gmail.com>
2023-10-31 21:43:50 +0700

    Create channel_group.lua

---
+++ TVSources/core/channel_group.lua
@@ -0,0 +1,7 @@
+local gr_filter = {
+['htv1_00']={'HTV1','HTV'},
+['htv2 hd - vie channel_00']={'HTV2 HD - Vie Channel','HTV'},
+['vtv1 hd_00']={'VTV1 HD','VTV'},
+['vtv2 hd_00']={'VTV2 HD','VTV'},
+} 
+ return gr_filter
